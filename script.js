9e0d45d51c937a89da8e19a73f64592f0f20bd42
khaled <khaledinat@gmail.com>
2021-07-13 14:11:44 +0200

    Async await javascript qui remplace: callback, promise

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// fetch api
+// async : pour dire que c'est asyncrone && wait: pour dire que ca prends de temps
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then (response => {
-      return response.json();
-  })
-  .then( json => {
-      console.log('Promise 1: ', json)
-      return fetch('https://jsonplaceholder.typicode.com/todos/'+json[0].userId);
-  })
-  .then (response => {
-    console.log('Promise 2: ', response)
-    return response.json();
-  })
-  .then( json => {
-    console.log('Promise 3: ', json)
-  })
-  .catch ( err => console.log(err));
+  const getTodos = async (url)=>{
+      //cette ligne  "await" pour dire que ca va prendre du gtemps mais qu'on il fini raméne moi la réponse
+   const reponse= await fetch(url)
+    const datas = await reponse.json();
+    console.log('Promise: 1', datas)
+    const reponse2 = await fetch(url +'/'+datas[0].userId);
+    const data = await reponse2.json();
+    console.log('Promise: 2', data);
+    const reponse3 = await fetch(url +'/'+datas[0].userId+'/comments');
+    const data3 = await reponse3.json();
+    console.log('Promise: 3', data3)
+  }
+
+  getTodos('https://jsonplaceholder.typicode.com/todos');
