0122844c59da5bebd5e5960f2eef79229a85810f
khaled <khaledinat@gmail.com>
2021-07-13 13:57:47 +0200

    fetch api

--- script.js
+++ script.js
@@ -1,50 +1,18 @@
-const getTodos = ( url ) => {
+// fetch api
 
-    return new Promise( (resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-            // console.log(request);
-        
-            if(request.readyState === 4 && request.status === 200) {
-    
-                const data = JSON.parse(request.responseText);
-    
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('Could not fectch data');
-            }
-        });
-        
-        request.open('GET', url);
-        
-        request.send();
-    });
-}
-
-
-
-getTodos('https://jsonplaceholder.typicode.com/todos')
-   .then( (data) =>{ 
-       console.log(data);
-       return getTodos('https://jsonplaceholder.typicode.com/todos/'+data[0].userId);     
-    })
-    .then( data=> {
-        console.log(data);
-        return getTodos('https://jsonplaceholder.typicode.com/todos/'+data.userId+'/comments');
-    })
-    .then( data=> 
-        console.log(data))
-   .catch( err => console.log(err));
-
-
-
-
-
-
-
-
-
-
-
-
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then (response => {
+      return response.json();
+  })
+  .then( json => {
+      console.log('Promise 1: ', json)
+      return fetch('https://jsonplaceholder.typicode.com/todos/'+json[0].userId);
+  })
+  .then (response => {
+    console.log('Promise 2: ', response)
+    return response.json();
+  })
+  .then( json => {
+    console.log('Promise 3: ', json)
+  })
+  .catch ( err => console.log(err));
