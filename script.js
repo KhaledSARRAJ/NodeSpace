9bd70daf24829b429532c4cb1bfef4a5f6805c8a
khaled <khaledinat@gmail.com>
2021-07-13 12:36:26 +0200

    PROMISE PRO trois appels

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-// const getTodos = (url, callback)=>{
-//     const request = new XMLHttpRequest();
-//  request.addEventListener('readystatechange', ()=>{
-//      // console.log(request);
-//      if(request.readyState === 4 && request.status===200){
-//          const data = JSON.parse(request.responseText);
-// callback(undefined,data);
-//     }else if(request.readyState===4){
-// callback('Could not fetch data',undefined);
-//      }
-//  });
- 
-//  request.open('GET', url);
-//  request.send(); 
-// }
+const getTodos = ( url ) => {
 
+    return new Promise( (resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-// getTodos('https://jsonplaceholder.typicode.com/todos',(err, data)=>{
-// console.log(data);
-// getTodos('https://jsonplaceholder.typicode.com/todos/'+data[0].userId,(err, data)=>{
-//     console.log(data);
-//     getTodos('https://jsonplaceholder.typicode.com/todos/'+data.userId+'/comments',(err, data)=>{
-
-//         console.log(data);
-
-//     });
-
-// });
-
-// });
-
-
-//promise exemple
-const getSomething = ()=>{
-    return new Promise((resolve, reject)=>{
-//network call
-resolve('Data received');
-//reject('Data not availlable')
+        request.addEventListener('readystatechange', () => {
+            // console.log(request);
+        
+            if(request.readyState === 4 && request.status === 200) {
+    
+                const data = JSON.parse(request.responseText);
+    
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('Could not fectch data');
+            }
+        });
+        
+        request.open('GET', url);
+        
+        request.send();
     });
 }
 
-//getSomething().then((data)=> console.log(data), (err)=>console.log(err));
-getSomething()
-.then(data=> console.log(data))
-.catch((err) => console.log(err));
+
+
+getTodos('https://jsonplaceholder.typicode.com/todos')
+   .then( (data) =>{ 
+       console.log(data);
+       return getTodos('https://jsonplaceholder.typicode.com/todos/'+data[0].userId);     
+    })
+    .then( data=> {
+        console.log(data);
+        return getTodos('https://jsonplaceholder.typicode.com/todos/'+data.userId+'/comments');
+    })
+    .then( data=> 
+        console.log(data))
+   .catch( err => console.log(err));
+
+
+
+
+
+
+
+
 
 
 
